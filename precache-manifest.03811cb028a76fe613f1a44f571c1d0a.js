217c39067205d90aa1d088418ddbf15fc192c306
Bohdan-lk <50949795+Bohdan-lk@users.noreply.github.com>
2020-05-03 13:34:46 +0300

    Updates

---
+++ precache-manifest.03811cb028a76fe613f1a44f571c1d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a612c4fd69e5889335c38277223843d",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "95a1cf0bfd56d81c013b",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.31e86769.chunk.css"
+  },
+  {
+    "revision": "40f57c8a91dba04c1812",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.73c7b42f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.73c7b42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a1cf0bfd56d81c013b",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.77f84986.chunk.js"
+  },
+  {
+    "revision": "cc5d00c14a25a053fbb7",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.e35af0b9.js"
+  }
+]);
